d2b5d5d3946836505195725fd774211a8c91698a
Tom O'Donnell <todonnel@stevens.edu>
2013-04-11 00:25:06 -0400

    fresh rails directory using a postgresql db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colab::Application.config.secret_token = '2bc00c892bc2c80058f8ba95aef8977ce88f689485af76a0d801b0c97cf0b7cae2d9bd5c2d26b0820cee7f0ca894cc76fecfee67fc12950364e32a510a31036e'
