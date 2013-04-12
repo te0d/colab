05b8940013b928af643b29e58013e8c78ea0a394
Tom O'Donnell <todonnel@stevens.edu>
2013-04-11 22:13:14 -0400

    installed and configured devise (no model yet); scaffolded group

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Colab
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Devise suggest turning off initializing assets on precompile
+    config.assets.initialize_on_precompile = false
   end
 end
